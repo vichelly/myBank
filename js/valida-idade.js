21db9ef9a12aa72870b667855475bc6fa9b8054d
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2022-12-14 10:34:27 -0300

    att

--- js/valida-idade.js
+++ js/valida-idade.js
@@ -1,11 +1,13 @@
-export default function ehMaiordeIdade(campo){
+export default function ehMaiorDeIdade(campo) {
     const dataNascimento = new Date(campo.value);
-    validaIdade(dataNascimento);
+    if (!validaIdade(dataNascimento)) {
+        campo.setCustomValidity('O usuário não é maior de idade');
+    }
 }
 
 function validaIdade(data) {
     const dataAtual = new Date();
-    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate() )
+    const dataMais18 = new Date(data.getUTCFullYear() + 18, data.getUTCMonth(), data.getUTCDate());
 
     return dataAtual >= dataMais18;
 }
